5daa506b7f26169fcad706de329bec0fd8c0f019
Renan Mendes Carvalho <renan.carvalho@corp.globo.com>
2016-07-12 12:35:17 -0300

    Add first ignorant implementation based on descomplica calculator code snippet

--- src/__tests__/viewport-interpolator-test.js
+++ src/__tests__/viewport-interpolator-test.js
@@ -2,11 +2,28 @@ jest.unmock('../viewport-interpolator')
 
 import viewportInterpolator from '../viewport-interpolator'
 
-describe('viewportInterpolator()', () => {
-  let subject
+let subject
+
+describe('viewportInterpolator', () => {
+  beforeEach(() => {
+    subject = viewportInterpolator
+  })
 
   it('is a function', () => {
-    subject = viewportInterpolator
     expect(subject).toEqual(jasmine.any(Function))
+  })
+})
+
+describe('viewportInterpolator(...)', () => {
+  beforeEach(() => {
+    subject = viewportInterpolator([320, 16], [768, 18], [1360, 22], [1920, 30])
+  })
+
+  it('returns a function', () => {
+    expect(subject).toEqual(jasmine.any(Function))
+  })
+
+  it('interpolates for 320', () => {
+    expect(subject(320)).toBe('calc(0.4464285714285714vw + 14.571428571428571px)')
   })
 })
--- src/viewport-interpolator.js
+++ src/viewport-interpolator.js
@@ -1,3 +1,33 @@
-const viewportInterpolator = () => {}
+const viewportInterpolator = (...points) => (viewportWidth) => {
+  const viewportWidths = []
+  const sizes = []
+  points.forEach((point) => {
+    viewportWidths.push(point[0])
+    sizes.push(point[1])
+  })
+
+  const values = []
+
+  for (var i = 0; i < viewportWidths.length-1; i++) {
+    var x1 = viewportWidths[i]
+    var x2 = viewportWidths[i+1]
+
+    var y1 = sizes[i]
+    var y2 = sizes[i+1]
+
+    var a = (y2-y1) / (x2-x1)
+    var b = (x2*y1 - x1*y2) / (x2 - x1)
+
+    values.push(`calc(${a * 100}vw + ${b}px)`)
+  }
+
+  for (var i = 0; i < viewportWidths.length-1; i++) {
+    if (viewportWidth < viewportWidths[i]) {
+      return values[i-1]
+    }
+  }
+
+  return values
+}
 
 export default viewportInterpolator
