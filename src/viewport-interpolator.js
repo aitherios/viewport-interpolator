3fa98556775f86b3d32ca96e932f48d295aeecb7
Renan Mendes Carvalho <aitherios@gmail.com>
2016-07-13 00:37:02 -0300

    Calculate interpolations only once on interpolator construction

--- src/viewport-interpolator.js
+++ src/viewport-interpolator.js
@@ -1,13 +1,13 @@
-const viewportInterpolator = (...points) => (viewportWidth) => {
-  // sort
+const viewportInterpolator = (...points) => {
   let interpolationPoints = points.sort((a, b) => {
+    // sort
     if (a[0] < b[0]) { return -1 }
     if (a[0] > b[0]) { return 1 }
     return 0
   })
 
-  // create interpolation values
   interpolationPoints = interpolationPoints.reduce((prev, current, index) => {
+    // calculate the interpolation
     let array
 
     if (!Array.isArray(prev[0])) {
@@ -36,13 +36,15 @@ const viewportInterpolator = (...points) => (viewportWidth) => {
     interpolationPoints = [[0, 0, interpolationPoints[0][2]], ...interpolationPoints]
   }
 
-  // remove points greater then viewport
-  interpolationPoints = interpolationPoints.filter(
-    (interpolation) => interpolation[0] <= viewportWidth
-  )
+  return (viewportWidth) => {
+    // remove points greater then viewport
+    const remainingPoints = interpolationPoints.filter(
+      (interpolation) => interpolation[0] <= viewportWidth
+    )
 
-  // return last remaining point
-  return interpolationPoints[interpolationPoints.length - 1][2]
+    // return last remaining point
+    return remainingPoints[remainingPoints.length - 1][2]
+  }
 }
 
 export default viewportInterpolator
